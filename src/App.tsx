c419b12b79e99bc8e00cd21f73a1258a8af069ec
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-10-03 12:49:13 -0300

    add: comments to explain the behaviour of the component

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,28 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
+  // States
   const [newTask, setNewTask] = useState<string>('');
   const [taskList, setTaskList] = useState<string[]>([]);
   const [countTask, setCountTask] = useState<number>(0);
 
+  // Handle events
   const handleAddTask = () => {
-    if (newTask.trim() === '') return;
-    setTaskList((doneTask) => [...doneTask, newTask]);
-    setCountTask(countTask + 1);
-    setNewTask('');
+    // Manage when the user wants to add a new task
+    if (newTask.trim() === '') return; // If the value from the input is empty just return '' and don't add nothing
+    setTaskList((doneTask) => [...doneTask, newTask]); // Add the new task to the array
+    setCountTask(countTask + 1); // Increment the counter of tasks
+    setNewTask(''); // Reset the value for the new task to ''
   };
 
   const handleDeleteTask = (index: number) => {
-    setTaskList((tasks) => tasks.filter((value, i) => i != index));
-    setCountTask(countTask - 1);
+    // Control when the user wants to delete a task
+    setTaskList((tasks) => tasks.filter((value, i) => i != index)); // Use filter to search the task to delete with the index of the element to search
+    setCountTask(countTask - 1); // Decrement the counter of tasks
   };
 
   const handleDeleteAllTask = () => {
+    // Just reset the values for the states
     setTaskList([]);
     setCountTask(0);
   };
